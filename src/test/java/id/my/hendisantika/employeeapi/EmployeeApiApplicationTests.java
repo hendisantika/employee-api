fd14b47942652dabe201bd2379311444fbe99de0
hendisantika <hendisantika@yahoo.co.id>
2024-12-16 19:20:58 +0700

    Add Integration Test🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/employeeapi/EmployeeApiApplicationTests.java
+++ src/test/java/id/my/hendisantika/employeeapi/EmployeeApiApplicationTests.java
@@ -1,13 +1,29 @@
 package id.my.hendisantika.employeeapi;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.employeeapi.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mysql:9.1.0:///employeeDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class EmployeeApiApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        employeeRepository.deleteAll();
     }
 
 }
