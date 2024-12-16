8006e71a7ca765d0f54d2c077c9bbc71912b59ba
hendisantika <hendisantika@yahoo.co.id>
2024-12-16 19:06:08 +0700

    Add updateEmployee in EmployeeService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/employeeapi/controller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/employeeapi/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,11 @@ public class EmployeeController {
 
     @GetMapping("{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId) {
-        return new ResponseEntity<Employee>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
+        return new ResponseEntity<>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long employeeId, @RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.updateEmployee(employee, employeeId), HttpStatus.OK);
     }
 }
