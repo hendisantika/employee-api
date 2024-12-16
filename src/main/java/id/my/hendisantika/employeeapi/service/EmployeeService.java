655bb8b124638fea15501ba8eeafe7fa26338476
hendisantika <hendisantika@yahoo.co.id>
2024-12-16 19:02:31 +0700

    Add EmployeeService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/employeeapi/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.employeeapi.service;
+
+import id.my.hendisantika.employeeapi.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : employee-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/12/24
+ * Time: 19.01
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+}
